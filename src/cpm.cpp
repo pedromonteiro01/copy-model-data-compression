d27292a99015cab9b5c48428f2c926ba7d791693
Pedro Monteiro <pmapm@ua.pt>
2023-03-10 12:04:14 +0000

    save all previous positions instead of only the first one

--- src/cpm.cpp
+++ src/cpm.cpp
@@ -33,27 +33,29 @@ int main()
     // cout << "Hello, World!\n";
     int k = 5; // k is the sliding window
     string content = read_file("chry.txt"); // get file content
-    std::map<std::string, int> hash_map; // create hashmap to store K strings
-    for (int i = 0; i < content.size()-k+1; ++i)
+    std::map<std::string, std::vector<int>> hash_map; // create hashmap to store K strings and its positions
+    for (int i = 0; i < content.size() - k + 1; ++i)
     {
         std::string substring = content.substr(i, k);
         if (hash_map.find(substring) != hash_map.end())
         {
-            int first_position = hash_map[substring];
+            hash_map[substring].push_back(i);
             if (i + k < content.size())
             {
-                char next_char = content[i+k];
-                cout << "Hit: " << substring << " (first appearance " << first_position << ") " << "Next char: " << next_char << endl;
-            }
-            else
-            {
-                cout << "Hit: " << substring << " " << first_position << " " << endl;
+                char next_char = content[i + k];
+                cout << substring << " (previous positions: ";
+                for (int pos : hash_map[substring]) {
+                    if (pos != i) {
+                        cout << pos << ", ";
+                    }
+                }
+                cout << i << ") " << "Next char: " << next_char << endl;
             }
         }
         else
         {
             cout << substring << endl;
-            hash_map[substring] = i; // add to hashmap
+            hash_map[substring] = std::vector<int>{i}; // add K-string to hashmap
         }
     }
     return 0;
