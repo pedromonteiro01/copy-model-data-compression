b4d71607948aac32372f9153fb40514b9972e846
Pedro Monteiro <pmapm@ua.pt>
2023-03-10 11:06:50 +0000

    iterate string content storing K strings in an hashmap

--- src/cpm.cpp
+++ src/cpm.cpp
@@ -5,16 +5,20 @@
 #include <map>
 #include <cmath>
 #include <algorithm>
-using std::cout; using std::endl;
-using std::string; using std::reverse;
+using std::cout;
+using std::endl;
 using std::ifstream;
 using std::ios;
+using std::reverse;
+using std::string;
 
 // read content of file and store it in a string
-string read_file(string filename) {
+string read_file(string filename)
+{
     string content;
     ifstream input(filename);
-    if (input) {
+    if (input)
+    {
         input.seekg(0, ios::end);
         content.resize(input.tellg());
         input.seekg(0, ios::beg);
@@ -24,12 +28,24 @@ string read_file(string filename) {
     return content;
 }
 
-int main() {
-    //cout << "Hello, World!\n";
-    string content = read_file("chry.txt");
-    for (int i = 0; i < content.size(); i++) {
-        cout << content[i];
+int main()
+{
+    // cout << "Hello, World!\n";
+    int k = 5; // k is the sliding window
+    string content = read_file("chry.txt"); // get file content
+    std::map<std::string, int> hash_map; // create hashmap to store K strings
+    for (int i = 0; i < content.size()-k+1; ++i)
+    {
+        std::string substring = content.substr(i, k);
+        if (hash_map.find(substring) != hash_map.end())
+        {
+            cout << "Hit: " << substring << endl; // if it is already in the hashmap
+        }
+        else
+        {
+            cout << substring << endl;
+            hash_map[substring] = i; // add to hashmap
+        }
     }
-    cout << endl;
     return 0;
 }
