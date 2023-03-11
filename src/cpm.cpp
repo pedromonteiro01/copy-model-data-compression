c95c7e18348aae139667ec137b78c3df45fc1016
eduardofernandes11 <edu_fernandes_2001@live.com.pt>
2023-03-11 16:03:21 +0000

    add hits and fails

--- src/cpm.cpp
+++ src/cpm.cpp
@@ -28,28 +28,54 @@ string read_file(string filename)
     return content;
 }
 
+// string best_char(std::vector<std::string> vector, std::vector<int> pos)
+// {
+//     std::map<std::string, std::vector<int>> map;
+//     int c,a,t,g = 0;
+//     if (pos.size() > 1){
+//         for (int j = 0; j < pos.size(); j++){
+//             if (pos[j] + 1 < vector.size()){
+//                 char c = vector[pos[j]+1].back();
+//             }
+//         }
+//     }
+// }
+
 int main()
 {
     // cout << "Hello, World!\n";
     int k = 5; // k is the sliding window
+    int hits = 0; // hits
+    int fails = 0; // fails
     string content = read_file("test.txt"); // get file content
     std::map<std::string, std::vector<int>> hash_map; // create hashmap to store K strings and its positions
+    std::vector<std::string> my_vector; // create vector to store the chunks 
     for (int i = 0; i < content.size() - k + 1; ++i)
     {
         std::string substring = content.substr(i, k);
+        my_vector.push_back(substring);
         if (hash_map.find(substring) != hash_map.end())
         {
-            hash_map[substring].push_back(i);
+
+            int position = hash_map[substring].back();   // get the previous position of the chunk
+            hash_map[substring].push_back(i);   // add the chunk current position to the hashmap
             if (i + k < content.size())
             {
-                char next_char = content[i + k];
-                cout << substring << " (previous positions: ";
-                for (int pos : hash_map[substring]) {
-                    if (pos != i) {
-                        cout << pos << ", ";
+                if (position + 1 < my_vector.size())
+                {
+                    char next_char = my_vector[position + 1].back();
+                    cout << substring << " (previous positions: ";
+                    for (int pos : hash_map[substring]) {
+                        if (pos != i) {
+                            cout << pos << ", ";
+                        }
                     }
+                    cout << i << ") " << "Pred char: " << next_char << " - Correct char: " << content[i+k] << endl;
+                    if (next_char == content[i+k])
+                        hits++;
+                    else
+                        fails++;
                 }
-                cout << i << ") " << "Next char: " << next_char << endl;
             }
         }
         else
@@ -58,5 +84,7 @@ int main()
             hash_map[substring] = std::vector<int>{i}; // add K-string to hashmap
         }
     }
+    cout << "Hits: " << hits << endl;
+    cout << "Fails: " << fails << endl;
     return 0;
 }
