21e5d522a2ce396b3599927c8f74740d12fbe4a7
zepedrotrigo <josetrigo@ua.pt>
2023-03-16 18:32:25 +0000

    refactored code to be easier to understand; developed a new predict_char() function

--- src/cpm.cpp
+++ src/cpm.cpp
@@ -15,75 +15,95 @@ using std::string;
 // read content of file and store it in a string
 string read_file(string filename)
 {
-    string content;
+    string data;
     ifstream input(filename);
     if (input)
     {
         input.seekg(0, ios::end);
-        content.resize(input.tellg());
+        data.resize(input.tellg());
         input.seekg(0, ios::beg);
-        input.read(&content[0], content.size());
+        input.read(&data[0], data.size());
         input.close();
     }
-    return content;
+    return data;
 }
 
-// string best_char(std::vector<std::string> vector, std::vector<int> pos)
-// {
-//     std::map<std::string, std::vector<int>> map;
-//     int c,a,t,g = 0;
-//     if (pos.size() > 1){
-//         for (int j = 0; j < pos.size(); j++){
-//             if (pos[j] + 1 < vector.size()){
-//                 char c = vector[pos[j]+1].back();
-//             }
-//         }
-//     }
-// }
+// Returns the key with greatest int value in a map
+char get_map_max_value(std::map<char, int> mymap) {
+    char retval = '\0';
+    int max_count = 0;
+    
+    for (const auto& c : mymap) {
+        if (c.second > max_count) {
+            max_count = c.second;
+            retval = c.first;
+        }
+    }
+
+    return retval;
+}
+
+// Function to get the most frequent next character
+char predict_char(const std::vector<int> &positions, std::vector<std::string> &chunks) {
+    // Step 0: Initialize a counter for each symbol
+    std::map<char, int> char_counter;
+
+    for (const auto& pos : positions)
+    {
+        // validate if a next chunk exists
+        if (pos+1 >= chunks.size())
+            continue;
+
+        // Get last char of the next chunk
+        char next_char = chunks[pos + 1].back();
+
+        // Increment counter for that char
+        char_counter[next_char]++;
+    }
+
+    // Find and return the most frequent symbol
+    char most_frequent_char = get_map_max_value(char_counter);
+
+    return most_frequent_char;
+}
 
 int main()
 {
-    // cout << "Hello, World!\n";
     int k = 5; // k is the sliding window
     int hits = 0; // hits
     int fails = 0; // fails
-    string content = read_file("test.txt"); // get file content
-    std::map<std::string, std::vector<int>> hash_map; // create hashmap to store K strings and its positions
-    std::vector<std::string> my_vector; // create vector to store the chunks 
-    for (int i = 0; i < content.size() - k + 1; ++i)
-    {
-        std::string substring = content.substr(i, k);
-        my_vector.push_back(substring);
-        if (hash_map.find(substring) != hash_map.end())
-        {
+    string data = read_file("test.txt"); // get file data
+    std::map<std::string, std::vector<int>> chunk_positions; // create hashmap to store K strings and its positions
+    std::vector<std::string> chunks; // create vector to store the chunks
 
-            int position = hash_map[substring].back();   // get the previous position of the chunk
-            hash_map[substring].push_back(i);   // add the chunk current position to the hashmap
-            if (i + k < content.size())
-            {
-                if (position + 1 < my_vector.size())
-                {
-                    char next_char = my_vector[position + 1].back();
-                    cout << substring << " (previous positions: ";
-                    for (int pos : hash_map[substring]) {
-                        if (pos != i) {
-                            cout << pos << ", ";
-                        }
-                    }
-                    cout << i << ") " << "Pred char: " << next_char << " - Correct char: " << content[i+k] << endl;
-                    if (next_char == content[i+k])
-                        hits++;
-                    else
-                        fails++;
-                }
-            }
+    for (int i = 0; i < data.size() - k; ++i)
+    {
+        std::string current_chunk = data.substr(i, k);
+        chunks.push_back(current_chunk);
+        
+        // if current_chunk not in hashmap then add to hashmap and go to next iteration
+        if (chunk_positions.find(current_chunk) == chunk_positions.end())  {
+            chunk_positions[current_chunk] = std::vector<int>{i}; // add K-string to hashmap
+            continue;
         }
-        else
-        {
-            cout << substring << endl;
-            hash_map[substring] = std::vector<int>{i}; // add K-string to hashmap
-        }
+
+        // add the chunk's current position to the hashmap
+        chunk_positions[current_chunk].push_back(i);
+        
+        // if EOF reached then break
+        if (i + k >= data.size())
+            break;
+
+        // get the solution (last char of next chunk)
+        char solution = data[(i+k)-1];
+
+        // make a prediction for the last char of the next chunk
+        char prediction = predict_char(chunk_positions[current_chunk], chunks);
+
+        // update results
+        prediction == solution ? hits++ : fails++;
     }
+
     cout << "Hits: " << hits << endl;
     cout << "Fails: " << fails << endl;
     return 0;
